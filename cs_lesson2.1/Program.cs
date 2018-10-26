0dcd289999a3fcabdfd9a7cb2e41de41c61659fb
david <2657070472@qq.com>
2018-10-26 10:55:00 +0800

    变量，加减乘除余

--- cs_lesson2.1/Program.cs
+++ cs_lesson2.1/Program.cs
@@ -10,6 +10,26 @@ namespace cs_lesson2._1
     {
         static void Main(string[] args)
         {
+            double firstNumber, secondNumber;        //声明变量
+            string userName;
+            Console.WriteLine("Enter your name :");
+            userName = Console.ReadLine();                 //输入变量 
+            Console.WriteLine("Welcome {0} !",userName);
+            Console.WriteLine("Now give me a number :");
+            firstNumber = Convert.ToDouble( Console.ReadLine());    //输入变量，并转为double类型
+            Console.WriteLine("Now give me another number :");
+            secondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The sum of {0} and {1} is {2}.",      
+                firstNumber, secondNumber, firstNumber + secondNumber );  //加
+            Console.WriteLine("The result of subtracting {0} from {1} is {2}.",
+              firstNumber, secondNumber, firstNumber - secondNumber);     //减
+            Console.WriteLine("The product of {0} and {1} is {2}.",
+              firstNumber, secondNumber, firstNumber * secondNumber);     //乘
+            Console.WriteLine("The result of dividing {0} by {1} is {2}.", 
+              firstNumber, secondNumber, firstNumber / secondNumber);       //除
+            Console.WriteLine("The remainder after dividing {0} by {1} is {2}.",
+              firstNumber, secondNumber, firstNumber % secondNumber);    //余
+            Console.ReadKey();
         }
     }
 }
